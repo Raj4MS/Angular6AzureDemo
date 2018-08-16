6a71e91abf96c6eec61aa2a24b9568c3e91b0039
Raj kumar <rajkumar4543@gmail.com>
2018-08-16 20:18:37 +0530

    Added customer mdodule

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router, Event, NavigationStart, NavigationEnd, NavigationCancel, NavigationError } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Angular6AzureDemo';
+  loader = false;
+
+  constructor(private router: Router) {
+    this.router.events.subscribe(
+      (routerEvent) => {
+        this.checkRouterEvents(routerEvent);
+      }
+    );
+  }
+
+  checkRouterEvents(routerEvent: Event) {
+    if (routerEvent instanceof NavigationStart) {
+      this.loader = true;
+    }
+    if (routerEvent instanceof NavigationEnd ||
+      routerEvent instanceof NavigationCancel ||
+      routerEvent instanceof NavigationError) {
+      this.loader = false;
+    }
+  }
+
+
 }
+
--- src/app/home/dashboard.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    templateUrl: './dashboard.component.html'
-})
-export class DashboardComponent {
-    title = 'welcome to dashboard page';
-}
